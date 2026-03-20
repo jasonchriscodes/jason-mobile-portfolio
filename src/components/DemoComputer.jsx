a857aa87114a4d5976a26f2ea73393b7a126d1de
Jason Christian <jasonchristian1234@gmail.com>
2026-03-21 02:28:28 +1300

    feat: add 3d model

---
+++ src/components/DemoComputer.jsx
@@ -0,0 +1,124 @@
+import React, {useEffect, useRef} from 'react';
+import {useGLTF, useVideoTexture} from '@react-three/drei';
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+
+const DemoComputer = (props) => {
+    const group = useRef();
+    const {nodes, materials} = useGLTF('/models/computer.glb');
+    const txt = useVideoTexture(
+        props.texture ? props.texture : '/textures/project/project1.mp4'
+    );
+
+    useEffect(() => {
+        if (txt) {
+            txt.flipY = false;
+        }
+    }, [txt]);
+
+    useGSAP(() => {
+        if (group.current) {
+            gsap.from(group.current.rotation, {
+                y: Math.PI / 2,
+                duration: 1,
+                ease: 'power3.out',
+            });
+        }
+    }, [txt]);
+
+    return (
+        <group ref={group} {...props} dispose={null}>
+            <group name="Scene">
+                <mesh
+                    name="monitor-screen"
+                    castShadow
+                    receiveShadow
+                    geometry={nodes['monitor-screen'].geometry}
+                    material={nodes['monitor-screen'].material}
+                    position={[0.127, 1.831, 0.511]}
+                    rotation={[1.571, -0.005, 0.031]}
+                    scale={[0.661, 0.608, 0.401]}
+                >
+                    <meshBasicMaterial map={txt} toneMapped={false}/>
+                </mesh>
+
+                <group
+                    name="RootNode"
+                    position={[0, 1.093, 0]}
+                    rotation={[-Math.PI / 2, 0, -0.033]}
+                    scale={0.045}
+                >
+                    {/* model groups here */}
+                </group>
+
+                <group
+                    name="Monitor-B-_computer_0"
+                    position={[0.266, 1.132, 0.051]}
+                    rotation={[0, -0.033, 0]}
+                    scale={[0.042, 0.045, 0.045]}
+                >
+                    <mesh
+                        name="Monitor-B-_computer_0_1"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_1'].geometry}
+                        material={materials.computer}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_2"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_2'].geometry}
+                        material={materials.base__0}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_3"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_3'].geometry}
+                        material={materials.Material_36}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_4"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_4'].geometry}
+                        material={materials.Material_35}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_5"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_5'].geometry}
+                        material={materials.Material_34}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_6"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_6'].geometry}
+                        material={materials.keys}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_7"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_7'].geometry}
+                        material={materials.keys2}
+                    />
+                    <mesh
+                        name="Monitor-B-_computer_0_8"
+                        castShadow
+                        receiveShadow
+                        geometry={nodes['Monitor-B-_computer_0_8'].geometry}
+                        material={materials.Material_37}
+                    />
+                </group>
+            </group>
+        </group>
+    );
+};
+
+useGLTF.preload('/models/computer.glb');
+
+export default DemoComputer;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
-import React, {useState} from 'react'
+import React, {Suspense, useState} from 'react'
 import {myProjects} from "../constan/index.js";
+import {Canvas} from "@react-three/fiber";
+import {Center, OrbitControls} from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import DemoComputer from "../components/DemoComputer.jsx";
 
 const Projects = () => {
     const projectCount = myProjects.length;
@@ -61,8 +65,21 @@ const Projects = () => {
                         <button className={"arrow-btn"} onClick={() => handleNavigation('next')}>
                             <img src={"/assets/right-arrow.png"} alt={"right arrow"} className={"w-4 h-4"}/>
                         </button>
-
                     </div>
+                </div>
+                <div className={"border border-black-300 bg-black-200 rounded-lg h-96 md:h-full"}>
+                    <Canvas>
+                        <ambientLight intensity={Math.PI}/>
+                        <directionalLight position={[10, 10, 5]}/>
+                        <Center>
+                            <Suspense fallback={<CanvasLoader/>}>
+                                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                    <DemoComputer texture={currentProject.texture}/>
+                                </group>
+                            </Suspense>
+                        </Center>
+                        <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false}/>
+                    </Canvas>
                 </div>
             </div>
         </section>
